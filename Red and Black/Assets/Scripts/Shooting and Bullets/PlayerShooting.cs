156fe228c1fa38bf1263cc402932fedeebe06f2f
stolenlegos <stolenlegos@gmail.com>
2022-05-23 17:25:33 -0700

    nonsense

--- Red and Black/Assets/Scripts/Shooting and Bullets/PlayerShooting.cs
+++ Red and Black/Assets/Scripts/Shooting and Bullets/PlayerShooting.cs
@@ -5,13 +5,15 @@ using UnityEngine.InputSystem;
 
 public class PlayerShooting : MonoBehaviour
 {
-    [SerializeField] private GameObject bullet;
+    [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform origin;
 
     public void ShootBullet(InputAction.CallbackContext ctx) {
         if (ctx.performed) {
             Debug.Log("Player - Gun Noises");
-            Instantiate(bullet, origin);
+            GameObject bullet = Instantiate(bulletPrefab, origin);
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3 (Mouse.current.position.ReadValue().x, Mouse.current.position.ReadValue().y, Mathf.Abs(transform.position.z)));
+
         }
     }
 }
