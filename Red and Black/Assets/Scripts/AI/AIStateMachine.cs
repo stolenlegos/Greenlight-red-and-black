a273afef52ee9e04b8b68ba6ad7336b305d56a56
solidsmyth <74252115+solidsmyth@users.noreply.github.com>
2022-06-08 17:01:34 -0400

    ai patrol added

--- Red and Black/Assets/Scripts/AI/AIStateMachine.cs
+++ Red and Black/Assets/Scripts/AI/AIStateMachine.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public enum AIState
 {
@@ -12,26 +13,41 @@ public enum AIState
 
 public class AIStateMachine : MonoBehaviour
 {
-    //AI components
+    [Header("AI Components")]
     [SerializeField] private Rigidbody2D aiRB;
     [SerializeField] private CapsuleCollider2D aiCollider;
     [SerializeField] private GameObject visionCone;
     private MeshCollider visionConeCollider;
     [SerializeField] private PhysicsMaterial2D noFriction;
     [SerializeField] private PhysicsMaterial2D fullFriction;
-    //AIstates
+
+    [Header("AI States")]
     [SerializeField] private AIState aiState = AIState.PATROL;
-    //masks
+
+    [Header("Layer References")]
     [SerializeField] private LayerMask aiLayerMask;
-    //player references
+
+    [Header("Player References and Pathfinding")]
     [SerializeField] private Transform playerTarget;
-    //ai movement variables
+    [SerializeField] private float activateDistance = 50f;
+    [SerializeField] private float pathUpdateSeconds = 0.5f;
+
+    [Header("Movement Physics")]
     [SerializeField] private float moveSpeed = 4;
-    [SerializeField] private float asdf;
+    [SerializeField] private float nextWaypointDistance = 3f;
+    [SerializeField] private float jumpNodeHeightRequirement = 0.8f;
+    [SerializeField] private float jumpHeight = 3f;
+    [SerializeField] private float jumpCheckOffset = 0.1f;
+
+    [Header("Custom Behavior")]
+    [SerializeField] private bool followCheck;
+    [SerializeField] private bool jumpCheck;
+    [SerializeField] private bool isFacingRight;
+
     //ai path
-    
-    //ai bools
-    private bool isFacingRight;
+    private Path path;
+    private int currentWaypoint = 0;
+    Seeker seeker;
     //ai collider size
     private Vector2 aiColliderSize;
     //slope check variables
@@ -40,12 +56,21 @@ public class AIStateMachine : MonoBehaviour
     private float slopeSideAngle;
     private Vector2 slopeNormalPerpendicular;
     private bool isOnSlope;
+    //ai Patrol Positions
+    private Vector3 startPos;
+    [SerializeField] private float patrolPath = 4f;
+    private bool patrolSwitch = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        visionCone = this.gameObject.transform.GetChild(0).gameObject;
         aiColliderSize = aiCollider.size;
         visionConeCollider = visionCone.GetComponent<MeshCollider>();
+        seeker = GetComponent<Seeker>();
+        startPos = this.gameObject.transform.position;
+
+        InvokeRepeating("UpdatePath", 0f, pathUpdateSeconds);
     }
 
     // Update is called once per frame
@@ -57,11 +82,48 @@ public class AIStateMachine : MonoBehaviour
     {
         if(aiState == AIState.PATROL)
         {
-            //aiRB.velocity = ()
+            if(this.gameObject.transform.position.x >= startPos.x && !patrolSwitch)
+            {
+                aiRB.velocity = new Vector2(moveSpeed, aiRB.velocity.y);
+                if (isFacingRight)
+                {
+                    FlipCharacter();
+                }
+                if(this.gameObject.transform.position.x >= startPos.x + (patrolPath - .5f) && !patrolSwitch)
+                {
+                    patrolSwitch = true;
+                }
+            }
+            else if(this.gameObject.transform.position.x <= startPos.x + patrolPath && patrolSwitch)
+            {
+                aiRB.velocity = new Vector2(-moveSpeed, aiRB.velocity.y);
+                if(!isFacingRight)
+                {
+                    FlipCharacter();
+                }
+                if(this.gameObject.transform.position.x <= startPos.x && patrolSwitch)
+                {
+                    patrolSwitch = false;
+                }
+            }
+            else if(this.gameObject.transform.position.x <= startPos.x && !patrolSwitch)
+            {
+                aiRB.velocity = new Vector2(moveSpeed, aiRB.velocity.y);
+                if (isFacingRight)
+                {
+                    FlipCharacter();
+                }
+            }
+            if (TargetInDistance() && followCheck)
+            {
+                Debug.Log("ChaseandShootChange");
+                aiState = AIState.CHASEANDSHOOT;
+            }
         }
         else if (aiState == AIState.CHASEANDSHOOT)
         {
-
+            Debug.Log("Chasing");
+            PathFollow();
         }
         else if (aiState == AIState.SEARCH)
         {
@@ -180,6 +242,67 @@ public class AIStateMachine : MonoBehaviour
         else
         {
             aiRB.sharedMaterial = noFriction;
+        }
+    }
+    private void UpdatePath()
+    {
+        if(followCheck && TargetInDistance() && seeker.IsDone())
+        {
+            seeker.StartPath(aiRB.position, playerTarget.position, OnPathComplete);
+        }
+    }
+    private void PathFollow()
+    {
+        if(path != null)
+        {
+            return;
+        }
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            return;
+        }
+
+        IsGrounded();
+        // direction calculation
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - aiRB.position).normalized;
+        Vector2 force = direction * moveSpeed * Time.deltaTime;
+        //jump
+        if (jumpCheck && IsGrounded())
+        {
+            if(direction.y > jumpNodeHeightRequirement)
+            {
+                aiRB.AddForce(Vector2.up * moveSpeed * jumpHeight);
+            }
+        }
+        // movement
+        aiRB.AddForce(force);
+        //next waypoint
+        float distance = Vector2.Distance(aiRB.position, path.vectorPath[currentWaypoint]);
+        if(distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+
+        if(aiRB.velocity.x > 0.05f)
+        {
+            FlipCharacter();
+        }
+        else if(aiRB.velocity.x < -0.05f)
+        {
+            FlipCharacter();
+        }
+    }
+
+    private bool TargetInDistance()
+    {
+        return Vector2.Distance(transform.position, playerTarget.transform.position) < activateDistance;
+    }
+    private void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
         }
     }
 }
